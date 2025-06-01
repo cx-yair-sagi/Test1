74b542f51699aaa66cdeb989525dbae5377794bf
Yair Sagi <118170206+cx-yair-sagi@users.noreply.github.com>
2025-06-01 11:15:17 +0300

    Update SQL_Injection.java
    test 3

--- 1/SQL_Injection.java
+++ 1/SQL_Injection.java
@@ -8,4 +8,54 @@ String query = "SELECT * FROM users WHERE userid ='"+ userid + "'" + " AND passw
 Statement stmt = connection.createStatement();
 ResultSet rs = stmt.executeQuery(query);
 }
+   String dLookUp(java.sql.Statement stat, String table, String fName, String where) {
+    java.sql.Connection conn1 = null;
+    java.sql.Statement stat1 = null;
+    try {
+      conn1 = cn();
+      stat1 = conn1.createStatement();
+      java.sql.ResultSet rsLookUp = openrs( stat1, "SELECT " + fName + " FROM " + table + " WHERE " + where);
+      if (! rsLookUp.next()) {
+        rsLookUp.close();
+        stat1.close();
+        conn1.close();
+        return "";
+      }
+      String res = rsLookUp.getString(1);
+      rsLookUp.close();
+      stat1.close();
+      conn1.close();
+      return (res == null ? "" : res);
+    }
+    catch (Exception e) {
+      return "";
+    }
+  }
+
+  long dCountRec(java.sql.Statement stat, String table, String sWhere) {
+    long lNumRecs = 0;
+    try {
+      java.sql.ResultSet rs = stat.executeQuery("select count(*) from " + table + " where " + sWhere);
+      if ( rs != null && rs.next() ) {
+        lNumRecs = rs.getLong(1);
+      }
+      rs.close();
+    }
+    catch (Exception e ) {};
+    return lNumRecs;
+  }
+
+  String proceedError(javax.servlet.http.HttpServletResponse response, Exception e) {
+    return e.toString();
+  }
+
+  String[] getFieldsName ( java.sql.ResultSet rs ) throws java.sql.SQLException {
+    java.sql.ResultSetMetaData metaData = rs.getMetaData();
+    int count = metaData.getColumnCount();
+    String[] aFields = new String[count];
+    for(int j = 0; j < count; j++) {
+      aFields[j] = metaData.getColumnLabel(j+1);
+    }
+    return aFields;
+  }
 }
